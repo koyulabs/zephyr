7ab7429dae1ba4524a0cc384c24d7801dc73b082
Nate Dunn <hello@natedunn.net>
2021-01-30 19:42:18 -0600

    fix(vue): try to fix netlify build error

--- packages/vue-zephyr/vue.config.js
+++ packages/vue-zephyr/vue.config.js
@@ -2,7 +2,7 @@ const addVueRawPreLoader = require("vue-raw-pre");
 const path = require("path");
 
 module.exports = {
-  outputDir: ".build",
+  outputDir: "build",
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false
