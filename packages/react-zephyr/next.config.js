18ce3b0f5a5ad740da12062fc945a47145cab9ae
Nate Dunn <hello@natedunn.net>
2021-01-31 12:36:38 -0600

    chore(react): test fix netlify/next deploy

--- packages/react-zephyr/next.config.js
+++ packages/react-zephyr/next.config.js
@@ -1,8 +1,8 @@
 // next.config.js
 module.exports = {
   distDir: 'build',
-  i18n: {
-    locales: ['en', 'zh-CH', 'es'],
-    defaultLocale: 'en',
-  },
+  // i18n: {
+  //   locales: ['en', 'zh-CH', 'es'],
+  //   defaultLocale: 'en',
+  // },
 }
